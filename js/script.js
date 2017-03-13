c40d916a40a836f0634fe7dee7cb5d568b6f4f89
vitig <sarkvit@live.com>
2017-03-13 23:32:31 +0300

    Update

--- js/script.js
+++ js/script.js
@@ -1,26 +1,15 @@
 var cells = new Array(9);
-var winCombinations = [
-    [0, 1, 2],
-    [3, 4, 5],
-    [6, 7, 8],
-    [0, 3, 6],
-    [1, 4, 7],
-    [2, 5, 8],
-    [0, 4, 8],
-    [2, 4, 6]
-]
 for (var i = 0; i < cells.length; i++) {
     cells[i] = {
         id: i,
         player: '',
-        status: false
+        wcomb: false
     }
 }
 
 Vue.component('cell', {
     template: '#cell-template',
     props: ['clickable', 'linethrow'],
-    computed: {},
     data: function() {
         return { currentSign: '' }
     },
@@ -53,55 +42,60 @@ Vue.component('zero', {
 new Vue({
     el: '#app',
     data: {
-        numCell: cells,
+        cellArray: cells,
         cellStatus: true,
-        resMessage: ''
+        resMessage: '',
+        winCombinations: [
+            [0, 1, 2],
+            [3, 4, 5],
+            [6, 7, 8],
+            [0, 3, 6],
+            [1, 4, 7],
+            [2, 5, 8],
+            [0, 4, 8],
+            [2, 4, 6]
+        ],
     },
     methods: {
         aiMove: function() {
+            if (this.hasWinner()) {
+                return;
+            }
+            var n = this.aiTurn();
+            this.$children[n].setSign('zero');
+
+            this.hasWinner();
+        },
+        hasWinner: function() {
             var winner = this.checkField();
             if (winner.winner) {
                 this.endGame(winner);
-                return;
+                return true;
             } else if (this.fieldIsFull()) {
                 this.endGame();
-                return;
+                return true;
             }
-            var n = Math.floor(Math.random() * 9);
-            if (!this.numCell[n].player) {
-                this.$children[n].setSign('zero');
-            } else {
-                this.aiMove();
-            }
-            winner = this.checkField();
-            if (winner.winner) {
-                this.endGame(winner);
-                return;
-            } else if (this.fieldIsFull()) {
-                this.endGame();
-                return;
-            }
+            return false;
         },
         checkField: function() {
             var win = false;
             var i = 0;
             do {
-                win = this.checkWinCombo(winCombinations[i]);
+                win = this.checkWinCombo(this.winCombinations[i]);
                 i++;
-            } while (!win && (i < winCombinations.length));
-            return { winner: win, combo: winCombinations[i - 1] };
+            } while (!win && (i < this.winCombinations.length));
+            return { winner: win, combo: this.winCombinations[i - 1] };
         },
         checkWinCombo: function(cArr) {
-            var cellArray = this.numCell;
-            if ((cellArray[cArr[0]].player == 'cross') && (cellArray[cArr[1]].player == 'cross') && (cellArray[cArr[2]].player == 'cross') ||
-                (cellArray[cArr[0]].player == 'zero') && (cellArray[cArr[1]].player == 'zero') && (cellArray[cArr[2]].player == 'zero')) {
-                return cellArray[cArr[0]].player;
+            if ((cells[cArr[0]].player == 'cross') && (cells[cArr[1]].player == 'cross') && (cells[cArr[2]].player == 'cross') ||
+                (cells[cArr[0]].player == 'zero') && (cells[cArr[1]].player == 'zero') && (cells[cArr[2]].player == 'zero')) {
+                return cells[cArr[0]].player;
             }
             return false;
         },
         fieldIsFull: function() {
-            if (this.numCell[0].player && this.numCell[1].player && this.numCell[2].player && this.numCell[3].player && this.numCell[4].player &&
-                this.numCell[5].player && this.numCell[6].player && this.numCell[7].player && this.numCell[8].player) {
+            if (cells[0].player && cells[1].player && cells[2].player && cells[3].player && cells[4].player &&
+                cells[5].player && cells[6].player && cells[7].player && cells[8].player) {
                 return true;
             }
             return false;
@@ -110,13 +104,61 @@ new Vue({
             this.cellStatus = false;
             if (winner) {
                 for (var i = 0; i < winner.combo.length; i++) {
-                    this.numCell[winner.combo[i]].status = true;
+                    cells[winner.combo[i]].wcomb = true;
                 }
-                this.resMessage = (winner.winner == 'cross' ? 'You' : 'AI') + ' Win!';
+                this.resMessage = (winner.winner == 'cross' ? 'You' : 'Computer') + ' Win!';
             } else {
-                this.resMessage = 'Nobody win...';
+                this.resMessage = 'Draw';
             }
-            console.log('End Game');
+        },
+        aiTurn: function() {
+            var position = this.checkThird('zero');
+            if (position === false) {
+                position = this.checkThird('cross');
+            }
+            if (position === false) {
+                position = this.checkSecond();
+            }
+            if (position === false) {
+                position = this.aiRndTurn();
+            }
+            return position;
+        },
+        checkThird: function(symbol) {
+            for (var i = 0; i < this.winCombinations.length; i++) {
+                var cmb = this.winCombinations[i];
+                if (cells[cmb[0]].player == symbol && cells[cmb[1]].player == symbol && !cells[cmb[2]].player) {
+                    return cmb[2];
+                } else if (cells[cmb[0]].player == symbol && cells[cmb[2]].player == symbol && !cells[cmb[1]].player) {
+                    return cmb[1];
+                } else if (cells[cmb[1]].player == symbol && cells[cmb[2]].player == symbol && !cells[cmb[0]].player) {
+                    return cmb[0];
+                }
+            }
+            return false;
+        },
+        checkSecond: function() {
+            for (var i = 0; i < this.winCombinations.length; i++) {
+                var cmb = this.winCombinations[i];
+                if ((cells[cmb[0]].player == 'zero') && (!cells[cmb[1]].player) && (!cells[cmb[2]].player)) {
+                    return cmb[1];
+                } else if ((cells[cmb[1]].player == 'zero') && (!cells[cmb[2]].player) && (!cells[cmb[0]].player)) {
+                    return cmb[2];
+                } else if ((cells[cmb[1]].player == 'zero') && (!cells[cmb[0]].player) && (!cells[cmb[2]].player)) {
+                    return cmb[0];
+                } else if ((cells[cmb[2]].player == 'zero') && (!cells[cmb[1]].player) && (!cells[cmb[0]].player)) {
+                    return cmb[1];
+                }
+            }
+            return false;
+        },
+        aiRndTurn: function() {
+            var n = Math.floor(Math.random() * 9);
+            if (!cells[n].player) {
+                return n;
+            } else {
+                return (this.aiRndTurn());
+            }
         }
 
     }
